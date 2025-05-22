61dcb44f774e5fc913f1556c472029c90c3b62ae
nift4 <nift4@protonmail.com>
2025-05-22 18:42:35 +0200

    Formal API to allow apps to handle playback resumption themselves
    This could previously be achieved by inspecting stack trace of
    onPlaybackResumption() and then returning settable future that will
    never be set, but this API adds a cleaner way and makes the caveats
    of this clearer.
    
    Issue: https://github.com/androidx/media/issues/1764

--- libraries/session/src/main/java/androidx/media3/session/MediaLibrarySessionImpl.java
+++ libraries/session/src/main/java/androidx/media3/session/MediaLibrarySessionImpl.java
@@ -470,7 +470,7 @@ import java.util.concurrent.Future;
             ? checkNotNull(getMediaNotificationControllerInfo())
             : controller;
     ListenableFuture<MediaSession.MediaItemsWithStartPosition> future =
-        callback.onPlaybackResumption(instance, controller);
+        callback.onPlaybackResumption(instance, controller, false);
     Futures.addCallback(
         future,
         new FutureCallback<MediaSession.MediaItemsWithStartPosition>() {
