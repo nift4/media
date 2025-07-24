42ee039e5b196bb3401505e7ba657ff38000d749
nift4 <nift4@protonmail.com>
2025-07-24 16:34:06 +0200

    Make frequently called controller callbacks local binder aware
    Especially MediaBrowser will profit from this, as List<MediaItem>
    no longer has to be bundled if the caller is in the same process.

--- libraries/session/src/main/java/androidx/media3/session/MediaSessionStub.java
+++ libraries/session/src/main/java/androidx/media3/session/MediaSessionStub.java
@@ -2181,13 +2181,21 @@ import java.util.concurrent.ExecutionException;
 
     @Override
     public void onSessionResult(int sequenceNumber, SessionResult result) throws RemoteException {
-      iController.onSessionResult(sequenceNumber, result.toBundle());
+      iController.onSessionResult(
+          sequenceNumber,
+          iController instanceof MediaControllerStub
+              ? result.toBundleForLocalProcess()
+              : result.toBundle());
     }
 
     @Override
     public void onLibraryResult(int sequenceNumber, LibraryResult<?> result)
         throws RemoteException {
-      iController.onLibraryResult(sequenceNumber, result.toBundle());
+      iController.onLibraryResult(
+          sequenceNumber,
+          iController instanceof MediaControllerStub
+              ? result.toBundleForLocalProcess()
+              : result.toBundle());
     }
 
     @Override
@@ -2322,11 +2330,14 @@ import java.util.concurrent.ExecutionException;
         boolean canAccessTimeline,
         int controllerInterfaceVersion)
         throws RemoteException {
+      SessionPositionInfo filteredPositionInfo =
+          sessionPositionInfo.filterByAvailableCommands(
+              canAccessCurrentMediaItem, canAccessTimeline);
       iController.onPeriodicSessionPositionInfoChanged(
           sequenceNumber,
-          sessionPositionInfo
-              .filterByAvailableCommands(canAccessCurrentMediaItem, canAccessTimeline)
-              .toBundle(controllerInterfaceVersion));
+          iController instanceof MediaControllerStub
+              ? filteredPositionInfo.toBundleForLocalProcess()
+              : filteredPositionInfo.toBundle(controllerInterfaceVersion));
     }
 
     @Override
