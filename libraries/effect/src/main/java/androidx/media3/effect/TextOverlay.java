e2a70c99615d3de2f0c807352f232aff52631d37
ibaker <ibaker@google.com>
2025-08-06 12:13:47 -0700

    Migrate static imports from `Assertions` to Guava's `Preconditions`
    A later change will migrate non-static imports (where the diff in each
    file will be larger - and may be a good opportunity to switch those
    sites to a static import too).
    
    PiperOrigin-RevId: 791791355

--- libraries/effect/src/main/java/androidx/media3/effect/TextOverlay.java
+++ libraries/effect/src/main/java/androidx/media3/effect/TextOverlay.java
@@ -16,7 +16,7 @@
 package androidx.media3.effect;
 
 import static android.os.Build.VERSION.SDK_INT;
-import static androidx.media3.common.util.Assertions.checkNotNull;
+import static com.google.common.base.Preconditions.checkNotNull;
 import static java.lang.Math.ceil;
 
 import android.annotation.SuppressLint;
