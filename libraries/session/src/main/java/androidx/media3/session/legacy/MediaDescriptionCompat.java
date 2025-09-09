64e3d3e4fb61f4f61ce98c8f589a3f76b49bc9f7
rohks <rohks@google.com>
2025-09-09 07:00:22 -0700

    Remove `@UnstableApi` from restricted internal APIs.
    The `@UnstableApi` annotation is redundant for APIs that are already marked with `@RestrictTo`, as these APIs are not exposed to external developers.
    
    PiperOrigin-RevId: 804892179

--- libraries/session/src/main/java/androidx/media3/session/legacy/MediaDescriptionCompat.java
+++ libraries/session/src/main/java/androidx/media3/session/legacy/MediaDescriptionCompat.java
@@ -28,13 +28,11 @@ import android.os.Parcelable;
 import androidx.annotation.Nullable;
 import androidx.annotation.RequiresApi;
 import androidx.annotation.RestrictTo;
-import androidx.media3.common.util.UnstableApi;
 
 /**
  * A simple set of metadata for a media item suitable for display. This can be created using the
  * Builder.
  */
-@UnstableApi
 @RestrictTo(LIBRARY)
 @SuppressLint("BanParcelableUsage")
 public final class MediaDescriptionCompat implements Parcelable {
