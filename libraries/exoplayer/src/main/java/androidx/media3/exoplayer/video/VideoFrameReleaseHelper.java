5e30f1fe7aa13feb40e26242b1589798a73bb6f6
tonihei <tonihei@google.com>
2025-10-17 07:34:22 -0700

    Don't call Surface.setFrameRate method on invalid surfaces
    Even though we ignore the resulting error, this may happen if we
    handle measured surface rate changes after the surface has been
    released already, causing log spam.
    
    PiperOrigin-RevId: 820684246

--- libraries/exoplayer/src/main/java/androidx/media3/exoplayer/video/VideoFrameReleaseHelper.java
+++ libraries/exoplayer/src/main/java/androidx/media3/exoplayer/video/VideoFrameReleaseHelper.java
@@ -370,7 +370,8 @@ public final class VideoFrameReleaseHelper {
   private void updateSurfacePlaybackFrameRate(boolean forceUpdate) {
     if (SDK_INT < 30
         || surface == null
-        || changeFrameRateStrategy == C.VIDEO_CHANGE_FRAME_RATE_STRATEGY_OFF) {
+        || changeFrameRateStrategy == C.VIDEO_CHANGE_FRAME_RATE_STRATEGY_OFF
+        || !surface.isValid()) {
       return;
     }
 
@@ -397,7 +398,8 @@ public final class VideoFrameReleaseHelper {
     if (SDK_INT < 30
         || surface == null
         || changeFrameRateStrategy == C.VIDEO_CHANGE_FRAME_RATE_STRATEGY_OFF
-        || surfacePlaybackFrameRate == 0) {
+        || surfacePlaybackFrameRate == 0
+        || !surface.isValid()) {
       return;
     }
     surfacePlaybackFrameRate = 0;
