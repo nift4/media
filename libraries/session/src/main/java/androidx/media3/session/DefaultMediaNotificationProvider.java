7f93148155d3d83c703f69f926068ae20214bcbc
tonihei <tonihei@google.com>
2025-10-24 08:30:59 -0700

    Remove duplicate filtering
    The same filtering already happens inside the method getMediaButtons
    method below that calls into
    CommandButton.getCustomLayoutFromMediaButtonPreferences
    
    Similarly, the copying with slots already happens in the util method
    and doesn't have to be repeated in getMediaButtons.
    
    PiperOrigin-RevId: 823540821

--- libraries/session/src/main/java/androidx/media3/session/DefaultMediaNotificationProvider.java
+++ libraries/session/src/main/java/androidx/media3/session/DefaultMediaNotificationProvider.java
@@ -54,7 +54,6 @@ import androidx.media3.session.MediaStyleNotificationHelper.MediaStyle;
 import com.google.common.base.Supplier;
 import com.google.common.base.Suppliers;
 import com.google.common.collect.ImmutableList;
-import com.google.common.primitives.ImmutableIntArray;
 import com.google.common.util.concurrent.FutureCallback;
 import com.google.common.util.concurrent.Futures;
 import com.google.common.util.concurrent.ListenableFuture;
@@ -316,16 +315,6 @@ public class DefaultMediaNotificationProvider implements MediaNotification.Provi
       Callback onNotificationChangedCallback) {
     ensureNotificationChannel();
 
-    ImmutableList.Builder<CommandButton> mediaButtonPreferencesWithEnabledCommandButtonsOnly =
-        new ImmutableList.Builder<>();
-    for (int i = 0; i < mediaButtonPreferences.size(); i++) {
-      CommandButton button = mediaButtonPreferences.get(i);
-      if (button.sessionCommand != null
-          && button.sessionCommand.commandCode == SessionCommand.COMMAND_CODE_CUSTOM
-          && button.isEnabled) {
-        mediaButtonPreferencesWithEnabledCommandButtonsOnly.add(mediaButtonPreferences.get(i));
-      }
-    }
     Player player = mediaSession.getPlayer();
     NotificationCompat.Builder builder = new NotificationCompat.Builder(context, channelId);
     int notificationId = notificationIdProvider.getNotificationId(mediaSession);
@@ -337,7 +326,7 @@ public class DefaultMediaNotificationProvider implements MediaNotification.Provi
             getMediaButtons(
                 mediaSession,
                 player.getAvailableCommands(),
-                mediaButtonPreferencesWithEnabledCommandButtonsOnly.build(),
+                mediaButtonPreferences,
                 !Util.shouldShowPlayButton(
                     player, mediaSession.getShowPlayButtonIfPlaybackIsSuppressed())),
             builder,
@@ -481,10 +470,7 @@ public class DefaultMediaNotificationProvider implements MediaNotification.Provi
 
     ImmutableList.Builder<CommandButton> commandButtons = new ImmutableList.Builder<>();
     if (hasCustomBackButton) {
-      commandButtons.add(
-          customLayout
-              .get(nextCustomLayoutIndex++)
-              .copyWithSlots(ImmutableIntArray.of(CommandButton.SLOT_BACK)));
+      commandButtons.add(customLayout.get(nextCustomLayoutIndex++));
     } else if (playerCommands.containsAny(
         COMMAND_SEEK_TO_PREVIOUS, COMMAND_SEEK_TO_PREVIOUS_MEDIA_ITEM)) {
       commandButtons.add(
@@ -510,10 +496,7 @@ public class DefaultMediaNotificationProvider implements MediaNotification.Provi
       }
     }
     if (hasCustomForwardButton) {
-      commandButtons.add(
-          customLayout
-              .get(nextCustomLayoutIndex++)
-              .copyWithSlots(ImmutableIntArray.of(CommandButton.SLOT_FORWARD)));
+      commandButtons.add(customLayout.get(nextCustomLayoutIndex++));
     } else if (playerCommands.containsAny(COMMAND_SEEK_TO_NEXT, COMMAND_SEEK_TO_NEXT_MEDIA_ITEM)) {
       commandButtons.add(
           new CommandButton.Builder(CommandButton.ICON_NEXT)
@@ -522,8 +505,7 @@ public class DefaultMediaNotificationProvider implements MediaNotification.Provi
               .build());
     }
     for (int i = nextCustomLayoutIndex; i < customLayout.size(); i++) {
-      commandButtons.add(
-          customLayout.get(i).copyWithSlots(ImmutableIntArray.of(CommandButton.SLOT_OVERFLOW)));
+      commandButtons.add(customLayout.get(i));
     }
     return commandButtons.build();
   }
