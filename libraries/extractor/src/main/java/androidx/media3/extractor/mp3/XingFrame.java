b0bf1e78c801e5fd8ca66656efff719473bd1dc7
nift4 <nift4@protonmail.com>
2026-06-04 16:15:16 +0200

    Support ID3/RIFF INFO/ReplayGain metadata in WAV extractor
    Including flag for returning prior behavior if seeking to end of
    file to find metadata chunks there (as old WAV readers do not
    support chunks between format and data, metadata is often put after
    data) is undesirable.
    
    Currently sample_rf64 test fails due to seeking more than max int
    value, and I will need some guidance on how exactly you can seek
    after the data segment if it's more than integer max value, as I
    can neither skipFully() nor seek numbers that high.
    
    The RIFF INFO tag names have been pieced together from "Multimedia
    Programming Interface and Data Specifications 1.0" (1991), exiftool,
    Hydrogen Audio, FFmpeg and wavemetatools.
    
    Samples created from other samples are also included, these were
    made with Kid3 and Awave Audio.

--- libraries/extractor/src/main/java/androidx/media3/extractor/mp3/XingFrame.java
+++ libraries/extractor/src/main/java/androidx/media3/extractor/mp3/XingFrame.java
@@ -21,6 +21,7 @@ import androidx.media3.common.Metadata;
 import androidx.media3.common.util.ParsableByteArray;
 import androidx.media3.common.util.Util;
 import androidx.media3.extractor.MpegAudioUtil;
+import androidx.media3.extractor.metadata.ReplayGainInfo;
 
 /** Representation of a LAME Xing or Info frame. */
 /* package */ final class XingFrame {
@@ -36,43 +37,8 @@ import androidx.media3.extractor.MpegAudioUtil;
    */
   public final long dataSize;
 
-  /** Whether this frame is the LAME variant of a Xing frame, and hence has ReplayGain data. */
-  public final boolean hasReplayGain;
-
-  /**
-   * @see Mp3InfoReplayGain#peak
-   */
-  public final float replayGainPeak;
-
-  /**
-   * @see Mp3InfoReplayGain#field1Name
-   */
-  public final byte replayGainField1Name;
-
-  /**
-   * @see Mp3InfoReplayGain#field1Originator
-   */
-  public final byte replayGainField1Originator;
-
-  /**
-   * @see Mp3InfoReplayGain#field1Value
-   */
-  public final float replayGainField1Value;
-
-  /**
-   * @see Mp3InfoReplayGain#field2Name
-   */
-  public final byte replayGainField2Name;
-
-  /**
-   * @see Mp3InfoReplayGain#field2Originator
-   */
-  public final byte replayGainField2Originator;
-
-  /**
-   * @see Mp3InfoReplayGain#field2Value
-   */
-  public final float replayGainField2Value;
+  /** ReplayGain data. Only present if this frame is an Info or the LAME variant of a Xing frame. */
+  @Nullable public final ReplayGainInfo replayGain;
 
   /**
    * The number of samples to skip at the start of the stream, or {@link C#LENGTH_UNSET} if not
@@ -97,28 +63,14 @@ import androidx.media3.extractor.MpegAudioUtil;
       long frameCount,
       long dataSize,
       @Nullable long[] tableOfContents,
-      boolean hasReplayGain,
-      float replayGainPeak,
-      byte replayGainField1Name,
-      byte replayGainField1Originator,
-      float replayGainField1Value,
-      byte replayGainField2Name,
-      byte replayGainField2Originator,
-      float replayGainField2Value,
+      @Nullable ReplayGainInfo replayGain,
       int encoderDelay,
       int encoderPadding) {
     this.header = new MpegAudioUtil.Header(header);
     this.frameCount = frameCount;
     this.dataSize = dataSize;
     this.tableOfContents = tableOfContents;
-    this.hasReplayGain = hasReplayGain;
-    this.replayGainPeak = replayGainPeak;
-    this.replayGainField1Name = replayGainField1Name;
-    this.replayGainField1Originator = replayGainField1Originator;
-    this.replayGainField1Value = replayGainField1Value;
-    this.replayGainField2Name = replayGainField2Name;
-    this.replayGainField2Originator = replayGainField2Originator;
-    this.replayGainField2Value = replayGainField2Value;
+    this.replayGain = replayGain;
     this.encoderDelay = encoderDelay;
     this.encoderPadding = encoderPadding;
   }
@@ -153,30 +105,17 @@ import androidx.media3.extractor.MpegAudioUtil;
       frame.skipBytes(4); // Quality indicator
     }
 
-    boolean hasReplayGain = false;
-    float replayGainPeak = 0f;
-    byte replayGainField1Name = 0;
-    byte replayGainField1Originator = 0;
-    float replayGainField1Value = 0;
-    byte replayGainField2Name = 0;
-    byte replayGainField2Originator = 0;
-    float replayGainField2Value = 0;
-    int encoderDelay = C.LENGTH_UNSET;
-    int encoderPadding = C.LENGTH_UNSET;
+    @Nullable ReplayGainInfo replayGain = null;
+    int encoderDelay = 0;
+    int encoderPadding = 0;
     // Skip: version string (9), revision & VBR method (1), lowpass filter (1).
     int bytesToSkipBeforeReplayGain = 9 + 1 + 1;
     if (frame.bytesLeft() >= bytesToSkipBeforeReplayGain + 8) {
       frame.skipBytes(bytesToSkipBeforeReplayGain);
-      hasReplayGain = true;
-      replayGainPeak = frame.readFloat();
-      short field1 = frame.readShort();
-      replayGainField1Name = (byte) ((field1 >> 13) & 7);
-      replayGainField1Originator = (byte) ((field1 >> 10) & 7);
-      replayGainField1Value = ((field1 & 0x1ff) * ((field1 & 0x200) != 0 ? -1 : 1)) / 10f;
-      short field2 = frame.readShort();
-      replayGainField2Name = (byte) ((field2 >> 13) & 7);
-      replayGainField2Originator = (byte) ((field2 >> 10) & 7);
-      replayGainField2Value = ((field2 & 0x1ff) * ((field2 & 0x200) != 0 ? -1 : 1)) / 10f;
+      float peak = frame.readFloat();
+      int field1 = frame.readUnsignedShort();
+      int field2 = frame.readUnsignedShort();
+      replayGain = ReplayGainInfo.parse(peak, field1, field2);
 
       // Skip: encoding flags & ATH type (1), bitrate (1).
       int bytesToSkipBeforeEncoderDelayAndPadding = 1 + 1;
@@ -189,9 +128,7 @@ import androidx.media3.extractor.MpegAudioUtil;
     }
 
     return new XingFrame(
-        mpegAudioHeader, frameCount, dataSize, tableOfContents, hasReplayGain, replayGainPeak,
-            replayGainField1Name, replayGainField1Originator, replayGainField1Value,
-            replayGainField2Name, replayGainField2Originator, replayGainField2Value,
+        mpegAudioHeader, frameCount, dataSize, tableOfContents, replayGain,
             encoderDelay, encoderPadding);
   }
 
@@ -213,8 +150,8 @@ import androidx.media3.extractor.MpegAudioUtil;
 
   /** Provide the metadata derived from this Xing frame, such as ReplayGain data. */
   public @Nullable Metadata getMetadata() {
-    if (hasReplayGain) {
-      return new Metadata(new Mp3InfoReplayGain(this));
+    if (replayGain != null) {
+      return new Metadata(replayGain);
     }
     return null;
   }
